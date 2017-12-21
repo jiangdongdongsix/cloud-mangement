e5cbd87cca4dafd92672b2cbf88b6f307caedd15
LongSheLA <475992348@qq.com>
2017-12-21 14:26:28 +0800

    "修改局部样式"

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -208,7 +208,7 @@ module.exports = {
           {
             loader: require.resolve('less-loader'),
                 options: {
-                  modifyVars: { "@primary-color": "#001529" },
+                  modifyVars: { "@primary-color": "#0cc2aa" },
                 },
           },
         ],
